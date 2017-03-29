49e3587f10182acc0d278c34cad44ea0ac253565
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-03-28 23:54:45 -0700

    added webpack!!!

---
+++ README.md
@@ -0,0 +1,22 @@
+## Development
+
+This application uses webpack for building the modules and providing hot module
+replacement, as well as a dev server.
+
+To build the project:
+
+```sh
+npm run build
+```
+
+To run webpack in `watch` mode:
+
+```sh
+npm run watch
+```
+
+To run the devserver:
+
+```sh
+npm run dev
+```
---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('./index.html');
+let Elm = require('./Main');
+
+let apiKey = localStorage.getItem('apiKey');
+
+let app;
+
+if(apiKey){
+  app = Elm.Main.fullscreen({apiKey: apiKey});
+} else {
+  app = Elm.Main.fullscreen({apiKey: null});
+}
+
+app.ports.storeApiKey.subscribe(function(data){
+  localStorage.setItem('apiKey', data)
+});
